54a4393c9287d8e7f691c483882562da32f9f944
w1lqa <mrsocrushitel46@gmail.com>
2026-04-30 02:18:26 +0300

    added create-superuser endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.http import JsonResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -11,4 +13,10 @@ urlpatterns = [
     path('api/', include('merch.urls')),
     path('api/', include('music.urls')),
     path('api/', include('orders.urls')),
+
+  path('create-admin/', lambda request: (
+      get_user_model().objects.create_superuser('admin@example.com', 'admin123456'),
+      JsonResponse({'status': 'created'})
+  )[1]),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
