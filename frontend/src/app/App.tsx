4dab72cc1f91529155b7593568dcd917e7e3f48f
w1lqa <mrsocrushitel46@gmail.com>
2026-05-25 17:54:07 +0300

    changed logo && added foreground noise fx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,18 +3,29 @@ import './styles/global.css';
 import { MerchDetailsPage } from '@/pages/merch-details';
 import { HomePage } from '@/pages/home';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import GrainOverlay from '@/shared/ui/GrainOverlay';
 
 export default function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/merch/:id" element={<MerchDetailsPage />} />
-        </Routes>
-      </BrowserRouter>
+      <div
+        className='relative'
+      //     className="relative before:absolute before:top-0 before:left-0 before:w-full
+      //  before:h-full before:content-[''] before:opacity-[0.02] before:z-1000 before:pointer-events-none
+      //  before:bg-[url('https://www.ui-layouts.com/noise.gif')]"
+      >
+        <GrainOverlay />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/merch/:id" element={<MerchDetailsPage />} />
+          </Routes>
+        </BrowserRouter>
+
+      </div>
+
     </QueryClientProvider>
   );
 }
--- frontend/src/widgets/header/ui/assets/icons/Logo.tsx
+++ frontend/src/widgets/header/ui/assets/icons/Logo.tsx
@@ -1,29 +1,19 @@
+import type { ImgHTMLAttributes } from 'react';
+import LogoImg from './1logo.png';
 import clsx from 'clsx';
-import type { SVGProps } from 'react';
 
-const Logo = ({ className, ...props }: SVGProps<SVGSVGElement>) => {
-    return (
-        <svg
-            className={clsx(className, '')}
-            width={36} height={57} viewBox="0 0 36 57" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-              <path
-                d="M25.3064 31.9167V23.1306L10.6906 37.6157V37.6189L0 48.2108V57L10.6906 46.4049L25.3064 31.9167Z"
-                fill="currentColor"
-              />
-              <path
-                d="M25.3064 0L0 25.0833V35.744L25.3064 10.6638V0Z"
-                fill="currentColor"
-              />
-              <path
-                d="M10.6914 26.6458V35.7412V35.7443L25.3071 21.2623L36.0009 10.6641V1.5625L25.3071 12.1607L10.6914 26.6458Z"
-                fill="currentColor"
-              />
-              <path
-                d="M36.0009 22.8184L25.3071 33.4169L10.6914 47.8986V57.0002L36.0009 31.9168V22.8184Z"
-                fill="currentColor"
-              />
-            </svg>
-    );
+const Logo = ({ className, ...props }: ImgHTMLAttributes<HTMLImageElement>) => {
+  return (
+    <img
+      src={LogoImg}
+      alt="Logo"
+      className={clsx(
+        className,
+        'brightness-0 invert w-full h-full object-contain min-w-24'
+      )}
+      {...props}
+    />
+  );
 };
 
 export default Logo;
