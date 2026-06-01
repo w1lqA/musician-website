9066ab655b598eb0a1890039be35c39ddad52436
w1lqa <mrsocrushitel46@gmail.com>
2026-06-01 15:34:23 +0300

    removed abs url

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -1,12 +1,6 @@
 # core/utils.py
-from django.conf import settings
 
 def get_absolute_url(path):
-    """возвращает абсолютный url для медиафайлов"""
     if not path:
         return ''
-    if path.startswith('http://') or path.startswith('https://'):
-        return path
-    if path.startswith('/media/'):
-        return f"{settings.BASE_URL}{path}"
-    return f"{settings.BASE_URL}/media/{path}"
+    return path
